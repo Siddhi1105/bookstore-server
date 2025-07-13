90fa7eccbf09987b2e3a4ce9ea112564701f9dd6
Siddhi1105 <siddhi.nanaware2004@gmail.com>
2025-07-13 13:20:22 +0530

    upgrade

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ app.use(cors({
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI,/*  {
   useNewUrlParser: true,
   useUnifiedTopology: true
-})
+} */)
   
   .then(() => console.log('MongoDB Connected'))
   .catch((err) => console.error(err));
